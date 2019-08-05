3d5578c2fe460fd191169268590fad4d0c8dab2b
Julio Moreira B. Barros <julio.barros@movile.com>
2019-08-05 14:06:45 -0300

    make main simpler

--- src/main/kotlin/com/playkids/workshop/Main.kt
+++ src/main/kotlin/com/playkids/workshop/Main.kt
@@ -1,38 +1,12 @@
 package com.playkids.workshop
 
-import com.playkids.workshop.adapter.TwitterClient
-import com.playkids.workshop.logic.countWords
-import com.playkids.workshop.logic.getWordRanking
-import com.playkids.workshop.logic.topTweet
-import com.playkids.workshop.model.Tweet
-
 /**
  * @author Júlio Moreira Blás de Barros (julio.barros@movile.com)
  * @since 8/3/19
  */
 fun main() {
-    println("everything wired up!!")
-
-    val user = TwitterClient.getLoggedUser()
-
-    println(user)
-
-    val tweets = TwitterClient.getHomeTweets()
-
-    println("total tweets: ${tweets.size}")
-    println()
-
-    val ranking = getWordRanking(tweets)
-
-    ranking
-        .subList(0, 50)
-        .forEachIndexed { index, (word, count) ->
-            println("Rank ${index+1}: '$word' written $count times")
-        }
-
-    println()
-    topTweet(tweets.map { it to it.favoriteCount })
-        ?.run {
-            println("Top tweet: '$text' from '@${relatedUser.screenName}' with $favoriteCount likes")
-        }
+    println("""
+        Everything wired up!!
+        Hello, 🌎!!!
+    """.trimIndent())
 }
