3b0b31ad2bb685a7de8dd8f4f04b317435bbaaec
Adriano Belfort <adriano.belfort@playkids.com>
2019-08-06 14:52:14 -0300

    Refactor main for better readability and format

--- src/main/kotlin/com/playkids/workshop/Main.kt
+++ src/main/kotlin/com/playkids/workshop/Main.kt
@@ -3,57 +3,60 @@ package com.playkids.workshop
 import com.playkids.workshop.adapter.TwitterClient
 import com.playkids.workshop.logic.getWordRanking
 import com.playkids.workshop.logic.topTweet
-import kotlin.math.max
 
 /**
  * @author Júlio Moreira Blás de Barros (julio.barros@movile.com)
  * @since 8/3/19
  */
 fun main() {
-    println("everything wired up!!")
+    println("Everything wired up!!")
 
     val user = TwitterClient.getLoggedUser()
 
     println(user)
 
-    val tweets = TwitterClient.getHomeTweets(300)
-
-    val filteredTweets = tweets.filter { it.retweetedTweet == null }
-        .filter { it.quoteInformation == null }
-        .filter { it.replyInfo == null }
+    val tweets = TwitterClient
+        .getHomeTweets(300)
         .toList()
 
-    println("total tweets: ${filteredTweets.size}")
+    val filteredTweets = tweets
+        .filter { it.retweetedTweet == null }
+        .filter { it.quoteInformation == null }
+        .filter { it.replyInfo == null }
+
+    println("Total tweets: ${filteredTweets.size}")
     println()
 
     val ranking = getWordRanking(filteredTweets)
 
     ranking
-        .filter { it.first.isNotBlank() }
-        .filter { !it.first.isConnective() }
-        .subList(0, max(50, filteredTweets.size))
+        .filter { rankedWord -> rankedWord.first.isNotBlank() }
+        .filter { (word, _) -> !word.isConnective() }
+        .take(50)
         .forEachIndexed { index, (word, count) ->
-            println("Rank ${index+1}: '$word' written $count times")
+            println("Rank ${index + 1}: '$word' written $count times")
         }
 
     println()
-    topTweet(filteredTweets.map { it to it.favoriteCount })
-        ?.run {
-            println("Top tweet: '$text' from '@${relatedUser.screenName}' with $favoriteCount likes")
-        }
+    val tweetsWithFavoriteCount = filteredTweets.map { it to it.favoriteCount }
+    val topTweet = topTweet(tweetsWithFavoriteCount)
+
+    println(
+        "Top tweet: '${topTweet?.text}' from '@${topTweet?.relatedUser?.screenName}' " +
+                "with ${topTweet?.favoriteCount} likes"
+    )
 
     val userTweets = TwitterClient.getUserTweets("JulioMoreira_", 40)
-    println("Tweets from @juliomoreira_: size: ${userTweets.size} | ${
-        userTweets
-            .map { it.text }
-            .joinToString("\n\n\n")
-    }")
+    println("Tweets from @juliomoreira_: size: ${userTweets.size}")
+    println(userTweets.joinToString("\n\n\n") { it.text })
 }
 
 private fun String.isConnective(): Boolean {
-    return setOf( "de", "que", "no", "pra","na", "do", "da", "to", "and",
+    return setOf(
+        "de", "que", "no", "pra", "na", "do", "da", "to", "and",
         "the", "com", "for", "um", "in", "of", "em", "your", "with", "te",
         "se", "as", "meu", "por", "para", "tambm", "tem", "muito", "ele",
-        "sendo","sobre", "nas", "minha", "vc", "by", "os", "our", "its")
+        "sendo", "sobre", "nas", "minha", "vc", "by", "os", "our", "its"
+    )
         .contains(this.toLowerCase()) || this.length <= 1
 }
