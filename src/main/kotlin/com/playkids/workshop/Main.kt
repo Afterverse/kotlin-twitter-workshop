723aaec19a5dab63d5ff1bd7002b09d174d8ed53
Julio Moreira B. Barros <julio.barros@movile.com>
2019-08-06 12:21:28 -0300

    sandbox without logic

--- src/main/kotlin/com/playkids/workshop/Main.kt
+++ src/main/kotlin/com/playkids/workshop/Main.kt
@@ -1,9 +1,6 @@
 package com.playkids.workshop
 
 import com.playkids.workshop.adapter.TwitterClient
-import com.playkids.workshop.logic.getWordRanking
-import com.playkids.workshop.logic.topTweet
-import kotlin.math.max
 
 /**
  * @author Júlio Moreira Blás de Barros (julio.barros@movile.com)
@@ -11,43 +8,13 @@ import kotlin.math.max
  */
 fun main() {
     println("everything wired up!!")
+    println("Hello, 🌎!!")
 
     val user = TwitterClient.getLoggedUser()
 
     println(user)
 
     val tweets = TwitterClient.getHomeTweets(300)
-
-    val filteredTweets = tweets.filter { it.retweetedTweet == null }
-        .filter { it.quoteInformation == null }
-        .filter { it.replyInfo == null }
-        .toList()
-
-    println("total tweets: ${filteredTweets.size}")
-    println()
-
-    val ranking = getWordRanking(filteredTweets)
-
-    ranking
-        .filter { it.first.isNotBlank() }
-        .filter { !it.first.isConnective() }
-        .subList(0, max(50, filteredTweets.size))
-        .forEachIndexed { index, (word, count) ->
-            println("Rank ${index+1}: '$word' written $count times")
-        }
-
-    println()
-    topTweet(filteredTweets.map { it to it.favoriteCount })
-        ?.run {
-            println("Top tweet: '$text' from '@${relatedUser.screenName}' with $favoriteCount likes")
-        }
-
-    val userTweets = TwitterClient.getUserTweets("JulioMoreira_", 40)
-    println("Tweets from @juliomoreira_: size: ${userTweets.size} | ${
-        userTweets
-            .map { it.text }
-            .joinToString("\n\n\n")
-    }")
 }
 
 private fun String.isConnective(): Boolean {
